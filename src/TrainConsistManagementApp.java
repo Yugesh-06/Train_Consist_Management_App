1803dcf2c5302cd2ce3f7ec7fba04090461e5af8
Yugesh-26 <yugeshkumarmr06@gmail.com>
2026-04-08 16:32:24 +0530

    UC1: Sort Bogies by Capacity

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Bogie {
+    private String name;
+    private int capacity;
+
+    public Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void display() {
+        System.out.println("Bogie Type: " + name + " | Capacity: " + capacity);
+    }
+}
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        List<Bogie> bogieList = new ArrayList<>();
+
+        bogieList.add(new Bogie("Sleeper", 72));
+        bogieList.add(new Bogie("AC Chair", 56));
+        bogieList.add(new Bogie("First Class", 24));
+
+        bogieList.sort(Comparator.comparingInt(Bogie::getCapacity));
+
+        System.out.println("Passenger Bogies Sorted by Capacity:");
+
+        for (Bogie b : bogieList) {
+            b.display();
+        }
+    }
 }
